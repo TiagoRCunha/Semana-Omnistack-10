7a3e72011c42ca5df12104e62d10a0e6fbfc7302
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2020-01-16 09:28:47 -0300

    routes

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
+const app = express();
+const config = require("./config");
+
+mongoose.connect(config.confString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(cors({ origin: `http://localhost:3000` }));
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
